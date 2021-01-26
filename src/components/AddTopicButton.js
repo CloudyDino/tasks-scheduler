8443fca1a20354beb9c99ca6b593ee7499ee60a4
CloudyDino <amaanmarfatia@gmail.com>
2021-01-26 10:21:57 -0500

    Fix AddTopicButton JSX errors

--- src/components/AddTopicButton.js
+++ src/components/AddTopicButton.js
@@ -9,9 +9,9 @@ export default function AddTopicButton(props) {
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1"
       >
         <line x1="1.5" y1="12.5" x2="23.5" y2="12.5"></line>
         <line x1="12.5" y1="23.5" x2="12.5" y2="1.5"></line>
