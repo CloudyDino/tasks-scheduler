88b7206b6eaaf5806fc93da4dea6fa17a181aaa1
CloudyDino <amaanmarfatia@gmail.com>
2021-03-04 23:37:00 -0500

    Show window on tray click for now

--- electron/electron.js
+++ electron/electron.js
@@ -56,6 +56,9 @@ function createWindow() {
 function createTray() {
   tray = new Tray(getIconPath());
   tray.setToolTip("Task Scheduler");
+  tray.on("click", () => {
+    window.show();
+  })
   tray.setContextMenu(
     Menu.buildFromTemplate([
       {
